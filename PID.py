1fda26af9077b5c58520fec3961e51806f9d986c
Skiadas Giorgos <skiadas.giorgos04@gmail.com>
2022-08-23 15:02:01 +0300

    move PID to separate file, change gains to match ordinary PID controllers

--- PID.py
+++ PID.py
@@ -1,13 +1,12 @@
+import numpy as np
 class PID:
-    def __init__(self, K_p, K_d, T_s, T_i, T_d, B_reference, B_measured, errors, current):
+    def __init__(self, K_p, K_d,K_i, B_reference, B_measured, errors, current):
         self.K_p = 1
         self.K_d = 1
-        self.T_s = 1
-        self.T_i = 1
-        self.T_d = 1
+        self.K_i = 1
         self.B_reference = 1
         self.B_measured = 1
-        self.errors = [1, 1, 1]
+        self.errors = np.array([1, 1, 1])
         self.current = 1
 
     def get_reference_magnetic_field(self, B_reference):
@@ -24,9 +23,10 @@ class PID:
 
     def calculate_current(self):
         current = self.current + self.K_p * (self.errors[0] - self.errors[1]) + \
-                  (self.K_p * self.T_s / self.T_i) * self.errors[0] + \
-                  (self.K_p * self.T_d / self.T_s) * (self.errors[0] - 2 * self.errors[1] + self.errors[2])
+                  self.K_i * self.errors[0] + \
+                  self.K_d * (self.errors[0] - 2 * self.errors[1] + self.errors[2])
 
         self.current = current
 
         return self.current
+
