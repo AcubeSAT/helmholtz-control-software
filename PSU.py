cfc6d7561f8421d754bfa582b1bae9dbfa588cd1
Dimitrios Sourlantzis <jem2010@windowslive.com>
2022-08-25 21:23:37 +0300

    add spd control

--- PSU.py
+++ PSU.py
@@ -1,49 +1,127 @@
+import time
 import serial
+import pyvisa
+
 
 class PSU:
-    # RIGOL DP712
+    """
+    Power supply unit control software for Spacedot's Helmholtz cage.
+    """
 
-    def __init__(self, port='/dev/ttyUSB0', channel='CH1'):
+    def __init__(self, port='/dev/ttyUSB0', channel='CH1', model='DP712'):
         self.max_current = 3
         self.max_voltage = 50
-        self.channel = channel
 
-        self.port = port
-        self.serial_communication = serial.Serial(self.port)
+        assert model in ['DP712', 'SPD3303C']
 
+        self.model = model
+
+        if self.model == 'DP712':
+            assert channel == 'CH1'
+            self.channel = channel
+            self.port = port
+            try:
+                self.serial_communication = serial.Serial(self.port)
+            except:
+                raise Exception("Device not found")
+
+        else:
+            assert channel in ['CH1', 'CH2']
+            self.channel = channel
+            device = pyvisa.ResourceManager().list_resources()
+            self.visa_device = ups = pyvisa.ResourceManager().open_resource(device[0])
 
     def set_voltage_and_current(self, voltage, current):
-        command = (':APPLy '+self.channel+','+str(voltage)+','+str(current)+'\n').encode("utf-8")
+        """
+        Sets voltage and current of the DP712 power supply unit.
+        Args:
+            voltage: the desired voltage
+            current: the desired current
+        """
+        assert self.model == "DP712"
+        assert voltage <= self.max_voltage
+        assert current <= self.max_current
+
+        command = (':APPLy ' + self.channel + ',' + str(voltage) + ',' + str(current) + '\n').encode("utf-8")
         self.serial_communication.write(command)
 
     def set_voltage(self, voltage):
-        command = (':APPLy '+str(voltage)+'\n').encode("utf-8")
-        self.serial_communication.write(command)
+        """
+        Desired voltage setter.
+        Args:
+            voltage: the desired voltage
+        """
+        assert voltage <= self.max_voltage
+        if self.model == 'DP712':
+            command = (':APPLy ' + str(voltage) + '\n').encode("utf-8")
+            self.serial_communication.write(command)
+        else:
+            command = (self.channel + ':VOLT ' + str(voltage) + '\n')
+            self.visa_device.write(command)
 
     def set_current(self, current):
-        command = (':APPLy ,' + str(current) + '\n').encode("utf-8")
-        self.serial_communication.write(command)
+        """
+        Desired current setter.
+        Args:
+            current: the desire current
+        """
+        assert current <= self.max_current
+        if self.model == 'DP712':
+            command = (':APPLy ,' + str(current) + '\n').encode("utf-8")
+            self.serial_communication.write(command)
+        else:
+            command = (self.channel + ':CURR ' + str(current))
+            self.visa_device.write(command)
 
-    def get_requested_voltage(self, voltage, current):
-        command = ':VOLT?\n'.encode("utf-8")
-        self.serial_communication.write(command)
-        voltage = (self.serial_communication.readline()).decode("utf-8")
-        return voltage
+    def get_requested_quantity(self, request_command):
+        """
+        Request the current voltage or current of the PSU.
+        Args:
+            request_command: the command for voltage or current
 
-    def get_requested_current(self, voltage):
-        command = ':CURR?\n'.encode("utf-8")
-        self.serial_communication.write(command)
-        current = (self.serial_communication.readline()).decode("utf-8")
-        return current
+        Returns:
+            The value of the requested quantity
+        """
+        assert request_command in ['voltage', 'current']
 
-    def measure_voltage(self):
-        command = (':MEAS:VOLT? '+serial.channel+'\n').encode("utf-8")
-        self.serial_communication.write(command)
-        voltage = (self.serial_communication.readline()).decode("utf-8")
-        return voltage
+        if request_command == 'voltage':
+            command = ':VOLT?\n'.encode('utf-8')
+        else:
+            command = ':CURR?\n'.encode('utf-8')
 
-    def measure_current(self):
-        command = (':MEAS:CURR? '+serial.channel+'\n').encode("utf-8")
-        self.serial_communication.write(command)
-        current = (self.serial_communication.readline()).decode("utf-8")
-        return current
+        if self.model == 'DP712':
+            self.serial_communication.write(command)
+            return (self.serial_communication.readline()).decode("utf-8")
+        else:
+            self.visa_device.write(command.decode('utf-8'))
+            return self.visa_device.query(command.decode('utf-8'), delay=0.01)
+
+    def measure_value(self, request_command):
+        """
+        Measures the current voltage or current of the PSU.
+        Args:
+            request_command: the command for voltage or current
+
+        Returns:
+            The measured PSU value
+        """
+        assert request_command in ['voltage', 'current']
+        if request_command == 'voltage':
+            command = ':MEAS:VOLT?'.encode('utf-8')
+        else:
+            command = ':MEAS:CURR?'.encode('utf-8')
+
+        if self.model == 'DP712':
+            self.serial_communication.write(command)
+            return (self.serial_communication.readline()).decode("utf-8")
+        else:
+            self.visa_device.write(command.decode('utf-8'))
+            return self.visa_device.query(command.decode('utf-8'), delay=0.01)
+
+    def set_channel(self, channel):
+        """
+        Changes the channel of the PSU.
+        Args:
+            channel: the desired new channel
+        """
+        self.channel = channel
