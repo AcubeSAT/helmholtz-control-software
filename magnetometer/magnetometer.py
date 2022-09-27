046754735489b2ff7e780c4e7854061814b008c8
Dimitrios Sourlantzis <jem2010@windowslive.com>
2022-09-27 18:26:36 +0300

    add magnetometer class

--- magnetometer/magnetometer.py
+++ magnetometer/magnetometer.py
@@ -1,10 +1,23 @@
-import serial, time
+import time
+
+import serial
+
 
 class Magnetometer:
+    def __init__(self, port='/dev/ttyACM0'):
+        self.com = serial.Serial(port)
+        self.first_readings = True
 
-    def __init__(self, port='/dev/ttyUSB0'):
-        self.serial_communication = serial.Serial(self.port)
-        time.sleep(2)
-    
     def get_magnetic_field(self):
+        data = self.com.readline()
+        data = data[:-2].split(b" ")
 
+        if self.first_readings:
+            self.first_readings = False
+            pass
+
+        magnetic_field = [0, 0, 0]
+        for i, byte in enumerate(data):
+            magnetic_field[i] = float(byte)
+
+        return magnetic_field
