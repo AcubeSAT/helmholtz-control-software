4d4d4784bbfc7204fb3f1193c712e4ee7c67feba
Skiadas Giorgos <skiadas.giorgos04@gmail.com>
2022-11-03 15:48:34 +0200

    Add IIS2MDC functional drivers, delete old

--- magnetometer/IIS2MDC/IIS2MDC.ino
+++ magnetometer/IIS2MDC/IIS2MDC.ino
@@ -1,15 +1,45 @@
+/*
+   @file    IIS2MDC_DataLogTerminal.ino
+   @author  Frederic Pillon <frederic.pillon@st.com>
+   @brief   Example to use the IIS2MDC high-performance 3-axis magnetometer
+ *******************************************************************************
+   Copyright (c) 2021, STMicroelectronics
+   All rights reserved.
+
+   This software component is licensed by ST under BSD 3-Clause license,
+   the "License"; You may not use this file except in compliance with the
+   License. You may obtain a copy of the License at:
+                          opensource.org/licenses/BSD-3-Clause
+
+ *******************************************************************************
+*/
+
+
+// Includes
 #include <IIS2MDCSensor.h>
 
 #define SerialPort  Serial
 
-#define IIS2MDC_I2C_SCL     PH4
-#define IIS2MDC_I2C_SDA     PH5
-
+#if defined(ARDUINO_B_U585I_IOT02A)
+#define IIS2MDC_I2C_SCL     PC5
+#define IIS2MDC_I2C_SDA     PC4                                                   
+#else
+// Uncomment to set I2C pins to use else default instance will be used
+// #define IIS2MDC_I2C_SCL  PYn
+// #define IIS2MDC_I2C_SDA  PYn
+#endif
+#if defined(IIS2MDC_I2C_SCL) && defined(IIS2MDC_I2C_SDA)
 TwoWire dev_i2c(IIS2MDC_I2C_SDA, IIS2MDC_I2C_SCL);
+#else
+// X-NUCLEO-IKS02A1 uses default instance
+#define dev_i2c       Wire
+#endif
 
-IIS2MDCSensor magnetometer(&dev_i2c);
+IIS2MDCSensor Magneto(&dev_i2c);
 
 void setup() {
+  // Led
+  pinMode(LED_BUILTIN, OUTPUT);
   // Initialize serial for output
   SerialPort.begin(9600);
 
@@ -17,19 +47,28 @@ void setup() {
   dev_i2c.begin();
 
   // Initlialize component
-  magnetometer.begin();
-  magnetometer.Enable();
+  Magneto.begin();
+  Magneto.Enable();
 }
 
 void loop() {
-  // Read magnetometer
-  int32_t magnetic_field[3];
-  magnetometer.GetAxes(magnetic_field);
+  // Led blinking
+//  digitalWrite(LED_BUILTIN, HIGH);
+  delay(10); 
+//  digitalWrite(LED_BUILTIN, LOW);
+  delay(10);
 
-  SerialPort.print("Mag[mGauss]:");
-  Serial.print(magnetic_field[0]);
-  Serial.print(", ");
-  Serial.print(magnetic_field[1]);
-  Serial.print(", ");
-  Serial.println(magnetic_field[2]);
-}
+  // Read magnetometer
+  int32_t magnetometer[3];
+  Magneto.GetAxes(magnetometer);
+//
+//  SerialPort.print("Mag[uT]:");
+  Serial.print((float)magnetometer[0]/10,5);
+  Serial.print(" ");
+  Serial.print((float)magnetometer[1]/10,5);
+  Serial.print(" ");
+  Serial.print((float)magnetometer[2]/10,5);
+Serial.print(" ");
+  float ut;
+  ut = sqrt(pow(((float)((float)magnetometer[0]/10)),2) + pow(((float)((float)magnetometer[1]/10)),2)+ pow(((float)((float)magnetometer[2]/10)),2));
+  Serial.println(ut);}
