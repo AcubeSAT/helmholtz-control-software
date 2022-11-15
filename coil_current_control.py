update main interface, fix current calculation bug

coil_current_control.py
@@ -24,10 +24,10 @@ class coil_current_control:
 
             Returns: the current that needs to be applied
         """
-
-        self.current = ((self.desired_magnetic_field - self.initial_magnetic_field) * np.pi * self.length) / (
+        self.gamma = helmholtz_constants.beta / self.length
+        self.current = ((self.desired_magnetic_field - self.initial_magnetic_field) * 1e-6 * np.pi * self.length) / (
                 2 * constants.mu_0 * helmholtz_constants.wire_turns) * (
-                               (1 + helmholtz_constants.beta ** 2) * np.sqrt(2 + helmholtz_constants.beta ** 2)) / 2
+                               (1 + self.gamma ** 2) * np.sqrt(2 + self.gamma ** 2)) / 2
 
         assert abs(self.current) <= helmholtz_constants.PSU_max_current, "Current above max value"
 

main.py
@@ -25,7 +25,7 @@ def get_initial_magnetic_field(magnetometer):
 
 if __name__ == "__main__":
 
-    # desired_magnetic_field = get_desired_magnetic_field()
+    desired_magnetic_field = get_desired_magnetic_field()
     II2MDC = magnetometer.Magnetometer()
     # loop to get the correct magnetometer values
     for _ in range(3):
@@ -37,7 +37,7 @@ if __name__ == "__main__":
 
     helmholtz_constants.initial_magnetic_field['x'], helmholtz_constants.initial_magnetic_field['y'],helmholtz_constants.initial_magnetic_field['z'] = get_initial_magnetic_field(magnetometer=II2MDC)
     print(helmholtz_constants.initial_magnetic_field)
-    desired_magnetic_field =[1,1,1]
+    # desired_magnetic_field =[1,1,1]
     coils = np.array(
         [
             coil_current_control('x', initial_magnetic_field['x']),
@@ -75,24 +75,21 @@ if __name__ == "__main__":
         PID[i].set_initial_current(coils[i].get_current())
         PID[i].set_reference_magnetic_field(desired_magnetic_field[i])
 
-    coils[0].current = 0
-    coils[1].current = -0.8
-    coils[2].current = 1
 
     while 1:
         for i in range(3):
             time.sleep(0.1)
             coils[i].set_desired_magnetic_field(desired_magnetic_field[i])
-            # coils[i].set_current()
+            coils[i].set_current()
             if coils[i].axis == 'y':
                 SPD3303C.set_channel('CH1')
                 time.sleep(0.1)
                 SPD3303C.set_current(abs(coils[i].get_current()))
                 time.sleep(0.1)
                 if coils[i].get_current() >= 0:
-                    sent_sign.sent_sign(helmholtz_constants.y_sign['positive'])
-                elif coils[i].get_current() < 0:
                     sent_sign.sent_sign(helmholtz_constants.y_sign['negative'])
+                elif coils[i].get_current() < 0:
+                    sent_sign.sent_sign(helmholtz_constants.y_sign['positive'])
             elif coils[i].axis == 'z':
                 SPD3303C.set_channel('CH2')
                 time.sleep(0.1)
@@ -107,14 +104,19 @@ if __name__ == "__main__":
                 DP712.set_current(abs(coils[i].get_current()))
                 time.sleep(0.2)
                 if coils[i].get_current() >= 0:
-                    sent_sign.sent_sign(helmholtz_constants.x_sign['positive'])
-                elif coils[i].get_current() < 0:
                     sent_sign.sent_sign(helmholtz_constants.x_sign['negative'])
+                elif coils[i].get_current() < 0:
+                    sent_sign.sent_sign(helmholtz_constants.x_sign['positive'])
     #
-    #     while 1:
-    #         print(SPD3303C.measure_current())
-    #         time.sleep(0.1)
-    #         print(DP712.measure_current())
+        while 1:
+            # print(SPD3303C.measure_current())
+            # time.sleep(0.1)
+            # print(DP712.measure_current())
+            # time.sleep(.1)
+            mf = II2MDC.get_magnetic_field()
+            norm = np.sqrt(mf[0] ** 2 + mf[1] ** 2 + mf[2] ** 2)
+            print(II2MDC.get_magnetic_field(), " ", norm)
+            time.sleep(.1)
     # while 1:
     #     # TODO: get measurements from magnetometer
     #     for i in range(2):