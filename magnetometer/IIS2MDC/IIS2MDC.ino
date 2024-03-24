785e23e4c2ae7d61d6a122789b6c3a47a1d249b0
Panagiotis Vamvakas <pavamvak@gmail.com>
2024-03-24 22:45:26 +0200

    Modified pin definition.

--- magnetometer/IIS2MDC/IIS2MDC.ino
+++ magnetometer/IIS2MDC/IIS2MDC.ino
@@ -2,10 +2,20 @@
 
 #define SerialPort  Serial
 
-#define IIS2MDC_I2C_SCL     PH4
-#define IIS2MDC_I2C_SDA     PH5
-
+#if defined(ARDUINO_B_U585I_IOT02A)
+#define IIS2MDC_I2C_SCL     PC5
+#define IIS2MDC_I2C_SDA     PC4                                                   
+#else
+// Uncomment to set I2C pins to use else default instance will be used
+// #define IIS2MDC_I2C_SCL  PYn
+// #define IIS2MDC_I2C_SDA  PYn
+#endif
+#if defined(IIS2MDC_I2C_SCL) && defined(IIS2MDC_I2C_SDA)
 TwoWire dev_i2c(IIS2MDC_I2C_SDA, IIS2MDC_I2C_SCL);
+#else
+// X-NUCLEO-IKS02A1 uses default instance
+#define dev_i2c       Wire
+#endif
 
 IIS2MDCSensor magnetometer(&dev_i2c);
 
@@ -26,10 +36,9 @@ void loop() {
   int32_t magnetic_field[3];
   magnetometer.GetAxes(magnetic_field);
 
-  SerialPort.print("Mag[mGauss]:");
-  Serial.print(magnetic_field[0]);
-  Serial.print(", ");
+  SerialPort.print(magnetic_field[0]);
+  Serial.print(" ");
   Serial.print(magnetic_field[1]);
-  Serial.print(", ");
+  Serial.print(" ");
   Serial.println(magnetic_field[2]);
 }
