b89a420d1e507d8b25004f65ab765b1e7fecda85
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2024-04-24 16:10:09 +0300

    Create a file to reset the PSUs to zero, after a failed test

---
+++ clean_psu.py
@@ -0,0 +1,30 @@
+import time
+from PSU import PSU
+
+
+if __name__ == "__main__":
+
+    SPD3303C = PSU('CH2', 'SPD3303C')
+    time.sleep(0.5)
+    DP712 = PSU("CH1", 'DP712')
+    time.sleep(0.5)
+
+    SPD3303C.set_channel('CH1')
+    time.sleep(0.2)
+    SPD3303C.set_current(0)
+    time.sleep(0.2)
+    SPD3303C.set_voltage(30)
+    time.sleep(0.2)
+
+    SPD3303C.set_channel('CH2')
+    time.sleep(0.2)
+    SPD3303C.set_current(0)
+    time.sleep(0.2)
+    SPD3303C.set_voltage(30)
+    time.sleep(0.2)
+
+    time.sleep(0.2)
+    DP712.set_current(0)
+    time.sleep(0.2)
+    DP712.set_voltage(30)
+    time.sleep(0.2)
