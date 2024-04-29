9afb249ee16fa5c3d38f8ea6acc0c5a400af9afb
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2024-04-29 18:51:14 +0300

    make the code more reliable and add test and try and except

--- magnetometer/magnetometer.py
+++ magnetometer/magnetometer.py
@@ -4,7 +4,7 @@ import serial
 import numpy as np
 
 class Magnetometer:
-    def __init__(self, port='/dev/ttyACM0'):
+    def __init__(self, port='/dev/ttyACM1'):
         self.com = serial.Serial(port, baudrate=9600, timeout=2)
         self.first_readings = True
         self.last_magnetic_field = [0, 0, 0]
@@ -17,24 +17,27 @@ class Magnetometer:
         while True:
             self.com.write(b"\x00")
             data = self.com.readline()
-
+            # print(f"data is {data}")
             if data == b"" or data == b"0":
                 continue
             
             #TODO: try to empty the serial buffer with a better way
             # Read all available data to prevent buffer overflow
             while self.com.in_waiting:
-                data += self.com.readline()
+                _ = self.com.readline()
 
+            # print(f"data is {data}")
             data = data[:-2].split(b" ")
 
             magnetic_field = [0, 0, 0]
             for i, byte in enumerate(data):
                 try:
+                    # print(f"byte is {byte}")
                     magnetic_field[i] = float(byte)
                 except:
                     # If conversion fails, handle the error and retry
-                    print("Error: Unable to convert byte to float.")
+                    print(f"Error: Unable to convert byte to float. {byte}")
+                    # print(f"magnetic field is {magnetic_field[i]}")
                     break
             else:
                 # If all conversions were successful, update and return the magnetic field
