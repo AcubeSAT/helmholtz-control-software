c2e6a9c5b8d2560076e466ff172e4a5ae9c5bf90
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2024-06-13 12:24:24 +0300

    Change the whole code for the tested code from imucal

--- magnetometer/IIS2MDC/IIS2MDC.ino
+++ magnetometer/IIS2MDC/IIS2MDC.ino
@@ -1,86 +1,74 @@
-/*
-   @file    IIS2MDC_DataLogTerminal.ino
-   @author  Frederic Pillon <frederic.pillon@st.com>
-   @brief   Example to use the IIS2MDC high-performance 3-axis magnetometer
- *******************************************************************************
-   Copyright (c) 2021, STMicroelectronics
-   All rights reserved.
+#include <Adafruit_SensorLab.h>
 
-   This software component is licensed by ST under BSD 3-Clause license,
-   the "License"; You may not use this file except in compliance with the
-   License. You may obtain a copy of the License at:
-                          opensource.org/licenses/BSD-3-Clause
+#include <Adafruit_Sensor_Calibration.h>
 
- *******************************************************************************
-*/
+Adafruit_SensorLab lab;
 
+Adafruit_Sensor *mag = NULL;
+sensors_event_t mag_event;
 
-// Includes
-#include <IIS2MDCSensor.h>
-#define SerialPort  Serial
+int loopcount = 0;
 
-#if defined(ARDUINO_B_U585I_IOT02A)
-#define IIS2MDC_I2C_SCL     PC5
-#define IIS2MDC_I2C_SDA     PC4                                                   
-#else
-// Uncomment to set I2C pins to use else default instance will be used
-// #define IIS2MDC_I2C_SCL  PYn
-// #define IIS2MDC_I2C_SDA  PYn
-#endif
-#if defined(IIS2MDC_I2C_SCL) && defined(IIS2MDC_I2C_SDA)
-TwoWire dev_i2c(IIS2MDC_I2C_SDA, IIS2MDC_I2C_SCL);
-#else
-// X-NUCLEO-IKS02A1 uses default instance
-#define dev_i2c       Wire
-#endif
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial) delay(10);
 
-IIS2MDCSensor Magneto(&dev_i2c);
+  Serial.println(F("Sensor Lab - IMU Calibration!"));
+  lab.begin();
 
-void setup() {
-  // Led
-  pinMode(LED_BUILTIN, OUTPUT);
-  // Initialize serial for output
-  SerialPort.begin(9600);
+  Serial.println("Looking for a magnetometer");
+  mag = lab.getMagnetometer();
+  if (!mag) {
+    Serial.println(F("Could not find a magnetometer, skipping!"));
+  } else {
+    mag->printSensorDetails();
+  }
 
-  // Initialize bus interface
-  dev_i2c.begin();
-
-  // Initlialize component
-  Magneto.begin();
-  Magneto.Enable();
+//  Serial.println("Looking for a gyroscope");
+//  gyro = lab.getGyroscope();
+//  if (!gyro) {
+//    Serial.println(F("Could not find a gyroscope, skipping!"));
+//  } else {
+//    gyro->printSensorDetails();
+//  }
+//
+//  Serial.println("Looking for an accelerometer");
+//  accel = lab.getAccelerometer();
+//  if (!accel) {
+//    Serial.println(F("Could not find an accelerometer, skipping!"));
+//  } else {
+//    accel->printSensorDetails();
+//  }
 }
 
 void loop() {
-
-  // Read magnetometer
-  int32_t magnetometer[3];
-  Magneto.GetAxes(magnetometer);
-//
-//  SerialPort.print("Mag[uT]:");
-//  Serial.print((float)magnetometer[0]/10,5);
-//  Serial.print(" ");
-//  Serial.print((float)magnetometer[1]/10,5);
-//  Serial.print(" ");
-//  Serial.print((float)magnetometer[2]/10,5);
-//  Serial.println();
-    
-    uint8_t peos;
-    long t = millis();
-    
-    if(Serial.read()){
-    peos = Serial.readString().toInt();
-    
-    
-    if(peos == 0) {
-    Serial.print((float)magnetometer[0]/10,5);
-    Serial.print(" ");
-    Serial.print((float)magnetometer[1]/10,5);
-    Serial.print(" ");
-    Serial.println((float)magnetometer[2]/10,5);
-    }
-    }
-  
-//  float ut;
-//  ut = sqrt(pow(((float)((float)magnetometer[0]/10)),2) + pow(((float)((float)magnetometer[1]/10)),2)+ pow(((float)((float)magnetometer[2]/10)),2));
-//  Serial.println(ut);
+  if (mag && !mag->getEvent(&mag_event)) {
+    return;
   }
+
+  // 'Raw' values to match expectation of MOtionCal
+//  Serial.print("Raw:");
+//  Serial.print(int(accel_event.acceleration.x * 8192 / 9.8)); Serial.print(",");
+//  Serial.print(int(accel_event.acceleration.y * 8192 / 9.8)); Serial.print(",");
+//  Serial.print(int(accel_event.acceleration.z * 8192 / 9.8)); Serial.print(",");
+//  Serial.print(int(gyro_event.gyro.x * Adafruit_SensorLab::DEGREES_PER_RADIAN * 16)); Serial.print(",");
+//  Serial.print(int(gyro_event.gyro.y * Adafruit_SensorLab::DEGREES_PER_RADIAN * 16)); Serial.print(",");
+//  Serial.print(int(gyro_event.gyro.z * Adafruit_SensorLab::DEGREES_PER_RADIAN * 16)); Serial.print(",");
+//  Serial.print(int(mag_event.magnetic.x * 10)); Serial.print(",");
+//  Serial.print(int(mag_event.magnetic.y * 10)); Serial.print(",");
+//  Serial.print(int(mag_event.magnetic.z * 10)); Serial.println("");
+
+  // unified data
+//  Serial.print("Uni:");
+//  Serial.print(accel_event.acceleration.x); Serial.print(",");
+//  Serial.print(accel_event.acceleration.y); Serial.print(",");
+//  Serial.print(accel_event.acceleration.z); Serial.print(",");
+//  Serial.print(gyro_event.gyro.x, 4); Serial.print(",");
+//  Serial.print(gyro_event.gyro.y, 4); Serial.print(",");
+//  Serial.print(gyro_event.gyro.z, 4); Serial.print(",");
+  Serial.print(-mag_event.magnetic.x); Serial.print(" ");
+  Serial.print(mag_event.magnetic.y); Serial.print(" ");
+  Serial.print(-mag_event.magnetic.z); Serial.println("");
+
+//  Serial.end();
+}
