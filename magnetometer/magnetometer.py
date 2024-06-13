c5507c85de9123ba0671b75d821ec1a5a6809dd9
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2024-06-13 13:22:37 +0300

    clean up some code, mainly improve comments and remove unwanted print functions

--- magnetometer/magnetometer.py
+++ magnetometer/magnetometer.py
@@ -9,6 +9,7 @@ class Magnetometer:
         self.first_readings = True
         self.last_magnetic_field = [0, 0, 0]
         self.V = [-24, -1.725, 44.475]
+        # This W 3x3 matrix origins is still a mystery
         self.W = [[1.0017, -0.1847, 0.2167],
                   [-0.1240, -1.0088, -0.2867],
                   [-0.2709, -0.2597, 1.0310]]
@@ -17,27 +18,22 @@ class Magnetometer:
         while True:
             self.com.write(b"\x00")
             data = self.com.readline()
-            # print(f"data is {data}")
             if data == b"" or data == b"0":
                 continue
             
-            #TODO: try to empty the serial buffer with a better way
             # Read all available data to prevent buffer overflow
             while self.com.in_waiting:
                 _ = self.com.readline()
 
-            # print(f"data is {data}")
             data = data[:-2].split(b" ")
 
             magnetic_field = [0, 0, 0]
             for i, byte in enumerate(data):
                 try:
-                    # print(f"byte is {byte}")
                     magnetic_field[i] = float(byte)
                 except:
                     # If conversion fails, handle the error and retry
                     print(f"Error: Unable to convert byte to float. {byte}")
-                    # print(f"magnetic field is {magnetic_field[i]}")
                     break
             else:
                 # If all conversions were successful, update and return the magnetic field
