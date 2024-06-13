ee8358291c8bd53895473f0881c34ebe3d956de2
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2024-06-13 12:25:37 +0300

    change clibration values and baud rate

--- magnetometer/magnetometer.py
+++ magnetometer/magnetometer.py
@@ -4,11 +4,11 @@ import serial
 import numpy as np
 
 class Magnetometer:
-    def __init__(self, port='/dev/ttyACM1'):
-        self.com = serial.Serial(port, baudrate=9600, timeout=2)
+    def __init__(self, port='/dev/ttyACM0'):
+        self.com = serial.Serial(port, baudrate=115200, timeout=2)
         self.first_readings = True
         self.last_magnetic_field = [0, 0, 0]
-        self.V = [-12.6343, 18.1653, -7.3313]
+        self.V = [-24, -1.725, 44.475]
         self.W = [[1.0017, -0.1847, 0.2167],
                   [-0.1240, -1.0088, -0.2867],
                   [-0.2709, -0.2597, 1.0310]]
