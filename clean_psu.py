a65e3e8fc999768d27c73f26026d39178f3bc843
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2024-10-09 13:30:26 +0300

    Reset to clean every PSU

--- clean_psu.py
+++ clean_psu.py
@@ -4,8 +4,8 @@ from magnetometer import PNI_magnetometer
 
 if __name__ == "__main__":
 
-    # SPD3303C = PSU('CH2', 'SPD3303C')
-    # time.sleep(0.1)
+    SPD3303C = PSU('CH2', 'SPD3303C')
+    time.sleep(0.1)
     DP712 = PSU("CH1", 'DP712')
     time.sleep(0.1)
 
@@ -14,19 +14,19 @@ if __name__ == "__main__":
     DP712.set_voltage(0)
     time.sleep(0.1)
 
-    # SPD3303C.set_channel('CH1')
-    # time.sleep(0.1)
-    # SPD3303C.set_current(0)
-    # time.sleep(0.1)
-    # SPD3303C.set_voltage(0)
-    # time.sleep(0.1)
+    SPD3303C.set_channel('CH1')
+    time.sleep(0.1)
+    SPD3303C.set_current(0)
+    time.sleep(0.1)
+    SPD3303C.set_voltage(0)
+    time.sleep(0.1)
 
-    # SPD3303C.set_channel('CH2')
-    # time.sleep(0.1)
-    # SPD3303C.set_current(0)
-    # time.sleep(0.1)
-    # SPD3303C.set_voltage(0)
-    # time.sleep(0.1)
+    SPD3303C.set_channel('CH2')
+    time.sleep(0.1)
+    SPD3303C.set_current(0)
+    time.sleep(0.1)
+    SPD3303C.set_voltage(0)
+    time.sleep(0.1)
 
-    magnetometer = PNI_magnetometer.PNI_magnetometer(port='/dev/ttyUSB3')
-    magnetometer.close()
+    # magnetometer = PNI_magnetometer.PNI_magnetometer(port='/dev/ttyUSB3')
+    # magnetometer.close()
