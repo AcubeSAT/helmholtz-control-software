9896b7cc3a0a05065af9be66e28d76ac0e12d178
Panagiotis Karvounaris <karvounarispanagiotis@gmail.com>
2024-12-17 13:56:31 +0200

    More clean up

--- Current_Magnetic_Field_Transform.py
+++ Current_Magnetic_Field_Transform.py
@@ -21,25 +21,3 @@ def input_magnetic_field_output_current(magnetic_field_control, side_length):
     current = (magnetic_field_control * np.pi) / (constants.mu_0 * helmholtz_constants.wire_turns * g)
     
     return current
-
-def input_current_output_magnetic_field(current, side_length):
-    """
-        This function calculates the magnetic field created by a single current value.
-
-        :param current: the current creating a magnetic field
-        :param side_length: coil side length (assuming the coil is a perfect square)
-
-        :return: the magnetic field created by the given current
-
-    """
-
-    # distance between a pair of coils
-    distance = side_length * 0.545
-
-    # geometrical parameter dependent on the shape of coils
-    geo_param = (8 * (side_length ** 2)) / (((side_length ** 2) + (distance ** 2)) * math.sqrt(
-        2 * (side_length ** 2) + (distance ** 2)))
-
-    magnetic_field = (helmholtz_constants.MAG_PERM_AIR * helmholtz_constants.wire_turns * current * geo_param) / np.pi
-
-    return magnetic_field
